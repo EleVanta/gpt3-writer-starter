b1bbf13d606c5682526c8fafa573a5bc092bd3db
EleVanta <119390195+EleVanta@users.noreply.github.com>
2023-04-16 19:30:53 +0000

    ; change

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const callGenerateEndpoint = async () => {
           placeholder="Example: Breakfast, Vegan, 30mins"
           value={userInput}
           onChange={onUserChangedText}
-        />;
+        />
   <div className="prompt-buttons">
     <a 
       className={isGenerating ? 'generate-button loading' : 'generate button'} 
@@ -74,7 +74,7 @@ const callGenerateEndpoint = async () => {
       <div className="output">
         <div className="output-header-container">
           <div className="output-header">
-            <h3>Output</h3>
+            <h3>Recipe</h3>
           </div>
          </div>
          <div className="output-content">
